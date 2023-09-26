37eb58e62a688e1342873939181331038d7ce018
MinhMatMong <kurominhnhat@gmail.com>
2023-09-26 18:08:45 +0700

    add middlwware redirect if no user and fix sign up UI with zod validation

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -10,7 +10,6 @@ import {
   FormDescription,
   FormField,
   FormItem,
-  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
@@ -27,12 +26,11 @@ const SignUpSchema = z
       message: "Please enter a valid email.",
     }),
     password: z
-      .string()
-      .regex(
+      .string().regex(
         new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).{8,}$"),
         {
           message:
-            "Password must contain at least one uppercase letter, one lowercase letter, one number and one special character.",
+            "Password must contain uppercase, special character, and number.",
         }
       ),
     confirmPassword: z.string(),
@@ -59,131 +57,133 @@ export default function SignUp() {
   }
 
   return (
-    <main className="w-screen h-full bg-background flex flex-col relative items-center  overflow-x-hidden">
+    <main className="w-screen h-screen overflow-hidden  bg-background flex flex-col relative items-center ">
       <Image
         src={"/login-bg.png"}
         width={1700}
         height={910}
         alt="bg"
-        className="absolute w-[1710px] h-[1000px] max-w-none top-0"
+        className="absolute w-screen min-w-[1440px] h-screen min-h-[760px] top-0 select-none pointer-events-none"
       />
-      <Tilt
-        glareEnable={true}
-        tiltEnable={false}
-        glareMaxOpacity={0.3}
-        glareBorderRadius="24px"
-        glarePosition="all"
-        className="w-[560px] mt-20  h-fit bg-form rounded-3xl flex flex-col justify-center items-center p-16 "
-      >
-        <h1 className="text-4xl font-bold tracking-wider mb-4">
-          <span className="super uppercase mr-2 font-bold">Gbox</span> Sign Up 
-        </h1>
-        <p className="text-base text-card-foreground text-center mb-8">
-          Join our community and become a better gamer.
-        </p>
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="w-full space-y-4"
-          >
-            {" "}
-            <FormField
-              control={form.control}
-              name="username"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="Username"
-                      type="text"
-                      {...field}
-                      className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
-                    />
-                  </FormControl>
-                  <FormDescription>Enter your username.</FormDescription>
-                  <FormMessage className="text-red-500" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="Email"
-                      type="email"
-                      {...field}
-                      className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
-                    />
-                  </FormControl>
-                  <FormDescription>Enter your email address.</FormDescription>
-                  <FormMessage className="text-red-500" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="password"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="Password"
-                      type="password"
-                      {...field}
-                      className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
-                    />
-                  </FormControl>
-                  <FormDescription>Enter your password.</FormDescription>
-                  <FormMessage className="text-red-500" />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="confirmPassword"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="Confirm Password"
-                      type="password"
-                      {...field}
-                      className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
-                    />
-                  </FormControl>
-                  <FormDescription>Confirm your password.</FormDescription>
-                  <FormMessage className="text-red-500" />
-                </FormItem>
-              )}
-            />
-            <div className="w-full border-[1px] cursor-pointer hover:border-primary hover:text-primary py-3 justify-center items-center rounded-full flex border-white">
-              <span className="text-sm">Or continue with google</span>
-              <BsGoogle className="ml-2 text-xl" />
-            </div>
-            <div className="w-full border-[1px] cursor-pointer hover:border-primary hover:text-primary py-3 justify-center rounded-full flex border-white">
-              <span className="text-sm">Or continue with discord</span>
-              <BsDiscord className="ml-2 text-xl" />
-            </div>
-            <Button
-              type="submit"
-              className="w-full !mt-10 font-bold uppercase tracking-widest "
+      <div className="flex flex-col items-center overflow-y-scroll py-20">
+        <Tilt
+          glareEnable={true}
+          tiltEnable={false}
+          glareMaxOpacity={0.3}
+          glareBorderRadius="24px"
+          glarePosition="all"
+          className="w-[560px]   h-fit bg-form rounded-3xl flex flex-col justify-center items-center p-16  "
+        >
+          <h1 className="text-4xl font-bold tracking-wider mb-4">
+            <span className="super uppercase mr-2 font-bold">Gbox</span> Sign Up 
+          </h1>
+          <p className="text-base text-card-foreground text-center mb-8">
+            Join our community and become a better gamer.
+          </p>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="w-full space-y-4"
             >
-              Submit
-            </Button>
-          </form>
-        </Form>
-        <p className=" mt-3">Already have an account? <Link href={'sign-in'} className="text-primary">Sign in</Link></p>
-      </Tilt>
-      <p className="text-white max-w-[600px] z-10  mt-10  px-8 text-center">
-        By signing up, you agree to our{" "}
-        <span className="text-primary">Terms of Service</span> and{" "}
-        <span className="text-primary">Privacy Policy</span>. For information on
-        how we utilize cookies, please refer to our{" "}
-        <span className="text-primary"> Cookies Policy</span>.
-      </p>
+              {" "}
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Username"
+                        type="text"
+                        {...field}
+                        className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
+                      />
+                    </FormControl>
+                    <FormDescription>Enter your username.</FormDescription>
+                    <FormMessage className="text-red-500" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Email"
+                        type="email"
+                        {...field}
+                        className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
+                      />
+                    </FormControl>
+                    <FormDescription>Enter your email address.</FormDescription>
+                    <FormMessage className="text-red-500" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Password"
+                        type="password"
+                        {...field}
+                        className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
+                      />
+                    </FormControl>
+                    <FormDescription>Enter your password.</FormDescription>
+                    <FormMessage className="text-red-500" />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormControl>
+                      <Input
+                        placeholder="Confirm Password"
+                        type="password"
+                        {...field}
+                        className="bg-transparent placeholder:text-white text-lg text-white border-t-0 border-l-0 border-r-0 border-white rounded-none focus-visible:!ring-offset-0 focus-visible:border-b-primary focus-visible:placeholder:text-primary  focus-visible:!ring-0"
+                      />
+                    </FormControl>
+                    <FormDescription>Confirm your password.</FormDescription>
+                    <FormMessage className="text-red-500" />
+                  </FormItem>
+                )}
+              />
+              <div className="w-full border-[1px] cursor-pointer hover:border-primary hover:text-primary py-3 justify-center items-center rounded-full flex border-white">
+                <span className="text-sm">Or continue with google</span>
+                <BsGoogle className="ml-2 text-xl" />
+              </div>
+              <div className="w-full border-[1px] cursor-pointer  hover:border-primary hover:text-primary py-3 justify-center rounded-full flex border-white">
+                <span className="text-sm">Or continue with discord</span>
+                <BsDiscord className="ml-2 text-xl" />
+              </div>
+              <Button
+                type="submit"
+                className="w-full !mt-10 font-bold uppercase tracking-widest "
+              >
+                Submit
+              </Button>
+            </form>
+          </Form>
+          <p className=" mt-3">Already have an account? <Link href={'sign-in'} className="text-primary">Sign in</Link></p>
+        </Tilt>
+        <p className="text-white max-w-[600px] z-10  mt-10  px-8 text-center">
+          By signing up, you agree to our{" "}
+          <span className="text-primary">Terms of Service</span> and{" "}
+          <span className="text-primary">Privacy Policy</span>. For information on
+          how we utilize cookies, please refer to our{" "}
+          <span className="text-primary"> Cookies Policy</span>.
+        </p>
+      </div>
     </main>
   );
 }
