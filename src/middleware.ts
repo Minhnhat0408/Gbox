6ad9d4b856826edb58ea93c2be9f92e7df924f08
hieunmh <hieunm.pt@gmail.com>
2023-10-01 22:20:20 +0700

    fix middleware and public folder

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,5 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: [ '/((?!api|sign-in|sign-up|_next/static|_next/image|favicon.ico).*)',],
+  matcher: [ '/((?!api|sign-in|sign-up|_next/static|_next/image|images|favicon.ico).*)',],
 }
