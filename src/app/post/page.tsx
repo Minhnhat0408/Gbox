e57caf0faa9f36fc3a625558503d41fe2c2f385a
huy-2k2 <huydev2002@gmail.com>
2023-10-05 23:56:44 +0700

    tạo form post

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import PostDetail from "@/app/post/postDetail";
 import PostForm from "@/app/post/postForm";
@@ -6,19 +6,19 @@ import { usePopupControl } from "@/components/ui/popup";
 import { useRef } from "react";
 
 export default function Post() {
-  
   const openPostFormRef = useRef<HTMLButtonElement>(null);
 
-  const formControl = usePopupControl(false, openPostFormRef)
+  const formControl = usePopupControl(false, openPostFormRef);
 
   return (
-    <main className="w-screen h-full bg-background flex flex-col relative items-center overflow-hidden">
+    <main className="w-screen h-full bg-background flex flex-col relative items-center">
       <div className="container ">
         <PostDetail></PostDetail>
         <PostForm formControl={formControl}></PostForm>
       </div>
-      <button ref={openPostFormRef} onClick={() => formControl.setOpen()}>open form</button>
-
+      <button ref={openPostFormRef} onClick={() => formControl.setOpen()}>
+        open form
+      </button>
     </main>
   );
 }
