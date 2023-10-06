737b748ac680accbe1e9ba6074dc65f2a9919910
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-10-06 20:48:18 +0700

    remove use client of layout

--- src/providers/LayoutProvider.tsx
+++ src/providers/LayoutProvider.tsx
@@ -1,13 +1,7 @@
-"use client";
-
 import SideBarLeft from "@/components/sideBar/sidebar-left";
 import SideBarRight from "@/components/sideBar/sidebar-right";
-import { usePathname } from "next/navigation";
 
 function LayoutProvider({ children }: { children: React.ReactNode }) {
-  // Prevent modal open when SSR
-  const pathname = usePathname();
-
   return (
     <>
       {<SideBarLeft />}
