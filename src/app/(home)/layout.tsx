3459e444846c2e909ecdafbd697bb920bf94336c
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-10-11 01:29:31 +0700

    update sooner theme

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
           {children}
         </main>
         <SideBarRight />
-        <Toaster richColors />
+        <Toaster richColors theme="dark" />
       </UserProvider>
       {/* </TanstackQueryProvider> */}
     </SupabaseProvider>
