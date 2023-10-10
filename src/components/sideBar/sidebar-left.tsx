660fff3a1421041fd299ff0a1a537343410d9c4e
Nguyễn Thành Dũng <thelastofus2isnuts@gmail.com>
2023-10-10 22:11:36 +0700

    create post form + rename folder

--- src/components/sideBar/sidebar-left.tsx
+++ src/components/sideBar/sidebar-left.tsx
@@ -1,15 +1,21 @@
 "use client";
-import Image from "next/image";
 import { useState } from "react";
 import { cn } from "@/lib/utils";
 import { navigation } from "@/constants/nav";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
-import { BsPlusCircleFill } from "react-icons/bs";
-import { RiBallPenFill, RiSwordFill, RiCoinsFill } from "react-icons/ri";
+import { BsImages, BsPlusCircleFill } from "react-icons/bs";
+import { RiSwordFill } from "react-icons/ri";
 import { BiMenu } from "react-icons/bi";
 import useUpdateGameModal from "@/hooks/useUpdateGameModal";
-import { IoLogoGameControllerB,IoIosArrowForward } from "react-icons/io";
+import { IoLogoGameControllerB } from "react-icons/io";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../ui/tooltip";
+import { usePostFormModal } from "@/hooks/usePostFormModal";
 
 export default function SideBarLeft() {
   const [expand, setExpand] = useState(false);
@@ -19,12 +25,16 @@ export default function SideBarLeft() {
 
   const { onOpen } = useUpdateGameModal();
 
+  const { onOpen: openPostForm } = usePostFormModal();
+
   return (
-    <aside
-      className={cn("fixed xl:left-8 left-4 fade-in h-full py-6 z-50  ")}
-    >
-      <div className={cn("h-full bg-muted rounded-3xl flex relative xl:py-6 py-3  xl:px-4 px-2  ")}>
-        <div className="flex flex-col h-full  items-center">
+    <aside className={cn("fixed xl:left-8 left-4 fade-in h-full py-6 z-50  ")}>
+      <div
+        className={cn(
+          "h-full bg-muted rounded-3xl flex relative xl:py-6 py-3  xl:px-4 px-2  "
+        )}
+      >
+        <div className="flex flex-col items-center h-full">
           {/* <Image
             src="/images/logo.png"
             alt="logo"
@@ -34,13 +44,19 @@ export default function SideBarLeft() {
             className={cn("w-16 h-16 cursor-pointer")}
             onClick={() => setExpand(!expand)}
           /> */}
-            <div  className="w-16 2xl:text-[46px] text-4xl flex justify-center  2xl:py-2   cursor-pointer" onClick={() => setExpand(!expand)}>
-            <BiMenu  />
+          <div
+            className="w-16 2xl:text-[46px] text-4xl flex justify-center  2xl:py-2   cursor-pointer"
+            onClick={() => setExpand(!expand)}
+          >
+            <BiMenu />
+          </div>
+          {/* <div className="xl:h-8 xl:w-8 bg-primary xl:top-28 top-20 xl:-right-4 -right-3 absolute flex items-center justify-center w-6 h-6 text-xl rounded-full" onClick={() => {setExpand(!expand)}}>{expand ?<IoIosArrowBack/> :<IoIosArrowForward/>}</div> */}
 
-            </div>
-            {/* <div className="absolute text-xl xl:h-8 xl:w-8  h-6 w-6 bg-primary flex items-center justify-center rounded-full xl:top-28 top-20 xl:-right-4 -right-3" onClick={() => {setExpand(!expand)}}>{expand ?<IoIosArrowBack/> :<IoIosArrowForward/>}</div> */}
-             
-          <nav className={cn("flex flex-col   justify-center 2xl:mt-20  mt-8 2xl:gap-y-6 gap-y-4")}>
+          <nav
+            className={cn(
+              "flex flex-col   justify-center 2xl:mt-20  mt-8 2xl:gap-y-6 gap-y-4"
+            )}
+          >
             {navigation.map((item, ind) => (
               <Link
                 key={ind}
@@ -66,31 +82,59 @@ export default function SideBarLeft() {
               "flex-col flex h-full justify-end  pt-6 pb-4 gap-y-3 duration-500"
             )}
           >
-            <li
-              className={cn(
-                "2xl:text-3xl hover:bg-primary/70 cursor-pointer  text-xl duration-500 xl:p-2 p-1 bg-primary opacity-0 translate-y-20 delay-200 text-muted rounded-full ",
-                openTools && "translate-y-0 opacity-100"
-              )}
-            >
-              <RiSwordFill />
-            </li>
-            <li
-              className={cn(
-                "2xl:text-3xl hover:bg-primary/70 cursor-pointer  text-xl duration-500 xl:p-2 p-1 bg-primary opacity-0 translate-y-20 delay-100 text-muted rounded-full ",
-                openTools && "translate-y-0 opacity-100"
-              )}
-            >
-              <RiCoinsFill />
-            </li>
-            <li
-              onClick={onOpen}
-              className={cn(
-                "2xl:text-3xl hover:bg-primary/70 cursor-pointer  text-xl duration-500 xl:p-2 p-1 bg-primary opacity-0 translate-y-20 text-muted rounded-full ",
-                openTools && "translate-y-0 opacity-100"
-              )}
-            >
-              <IoLogoGameControllerB />
-            </li>
+            <TooltipProvider delayDuration={200}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <li
+                    className={cn(
+                      "2xl:text-3xl hover:bg-primary/70 cursor-pointer  text-xl duration-500 xl:p-2 p-1 bg-primary opacity-0 translate-y-20 delay-200 text-muted rounded-full ",
+                      openTools && "translate-y-0 opacity-100"
+                    )}
+                  >
+                    <RiSwordFill />
+                  </li>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Find teammate</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider delayDuration={200}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <li
+                    className={cn(
+                      "2xl:text-3xl hover:bg-primary/70 cursor-pointer  text-xl duration-500 xl:p-2 p-1 bg-primary opacity-0 translate-y-20 delay-100 text-muted rounded-full ",
+                      openTools && "translate-y-0 opacity-100"
+                    )}
+                    onClick={openPostForm}
+                  >
+                    <BsImages />
+                  </li>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Post game review</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+            <TooltipProvider delayDuration={200}>
+              <Tooltip>
+                <TooltipTrigger>
+                  <li
+                    onClick={onOpen}
+                    className={cn(
+                      "2xl:text-3xl hover:bg-primary/70 cursor-pointer  text-xl duration-500 xl:p-2 p-1 bg-primary opacity-0 translate-y-20 text-muted rounded-full ",
+                      openTools && "translate-y-0 opacity-100"
+                    )}
+                  >
+                    <IoLogoGameControllerB />
+                  </li>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>Update game library</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </ul>
           <button
             className="2xl:text-5xl text-4xl z-10 hover:bg-primary/20 p-2 mt-auto duration-500 rounded-lg border-[1px] cursor-pointer border-dotted border-primary text-primary "
@@ -113,7 +157,11 @@ export default function SideBarLeft() {
           >
             Gbox
           </h1>
-          <nav className={cn("flex flex-col   justify-center 2xl:mt-20 mt-8 2xl:gap-y-6 gap-y-4")}>
+          <nav
+            className={cn(
+              "flex flex-col   justify-center 2xl:mt-20 mt-8 2xl:gap-y-6 gap-y-4"
+            )}
+          >
             {navigation.map((item, ind) => (
               <Link
                 key={ind}
