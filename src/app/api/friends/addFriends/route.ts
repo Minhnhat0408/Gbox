7ebd385f8d9c38a76035078950ddd9e408f1c28b
tronglinh23 <tronglinh230203@gmail.com>
2023-10-19 14:31:47 +0700

    Resolve conflict

--- src/app/api/friends/addFriends/route.ts
+++ src/app/api/friends/addFriends/route.ts
@@ -2,6 +2,7 @@ import { Database } from "@/types/supabaseTypes";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function POST(req: NextRequest) {
   // get sender_uuid
--- src/app/api/userSearch/route.ts
+++ src/app/api/userSearch/route.ts
@@ -2,6 +2,7 @@ import { Database } from "@/types/supabaseTypes";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(req: NextRequest) {
   const searchParams = req.nextUrl.searchParams;
