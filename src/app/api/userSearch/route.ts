b1d36b878f039a73fa11b7e2239b6024d904d0df
tronglinh23 <tronglinh230203@gmail.com>
2023-10-22 12:19:35 +0700

    update user search api

--- src/app/api/userSearch/route.ts
+++ src/app/api/userSearch/route.ts
@@ -14,46 +14,16 @@ export async function GET(req: NextRequest) {
   if (!userID) {
     return NextResponse.json({ status: 500, data: {} });
   }
-
   const supabaseClient = createRouteHandlerClient({ cookies });
-  const query = `*, 
-    sender:sender_receivers!sender_receivers_receiver_id_fkey(*), 
-    receiver:sender_receivers!sender_receivers_sender_id_fkey(*)
-  `;
   
-  const { data, error } = await supabaseClient
-  .from("profiles")
-  .select(query)
-  .or(`sender_id.eq.${userID},receiver_id.eq.${userID}`, {
-    foreignTable: "sender_receivers",
-  })
-  .ilike("name", `${searchWord}%`);
-
-  // add status to object
-  // 1. friend => view profile
-  // 2. waiting to accept
-  // 3. waiting to be accepted
-  // 4. not friend
-  if (!data || data.length === 0) {} 
-  else {
-    for (let i = 0; i < data.length; i++) {
-      if (data[i].id === userID) {
-        data[i].status = 1;
-      } else if (data[i].sender.length !== 0) {
-        data[i].status = 3;
-        if(data[i].sender[0].is_accepted === true) {
-          data[i].status = 1;
-        }
-      } else if (data[i].receiver.length !== 0) {
-        data[i].status = 2;
-        if(data[i].receiver[0].is_accepted === true) {
-          data[i].status = 1;
-        }
-      } else {
-        data[i].status = 4;
-      }
+  const { data, error } = await supabaseClient.rpc(
+    "get_friend_request_status",
+    {
+      searchword: searchWord,
+      currentuserid: userID,
     }
-  }
+  );
+  console.log(data, error);
 
   if (error) {
     console.error(error);
