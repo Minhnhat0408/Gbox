14163553e03e1ca51bb4ba5215cf5ee7112a14ce
tronglinh23 <tronglinh230203@gmail.com>
2023-11-07 22:16:32 +0700

    Fix deployment fail

--- src/hooks/useTypingDictator.ts
+++ src/hooks/useTypingDictator.ts
@@ -4,21 +4,23 @@ import { MessageType } from "@/types/supabaseTableType";
 import { useSessionContext } from "@supabase/auth-helpers-react";
 import { RealtimeChannel } from "@supabase/supabase-js";
 import { useCallback, useEffect, useRef, useState } from "react";
+const throttle = require('lodash.throttle');
 
-type TypingIndicatorProps = {
-    roomId: string;
-    userId: string;
-};
+type payloadType = {
+    userAva: string;
+    type:string;
+    event:string;
+  };
 
 
-export const useTypingIndicator = ({ roomId, userId }: TypingIndicatorProps) => {
+export const useTypingIndicator = ({userAva} : {userAva:string}) => {
     const [isTyping, setIsTyping] = useState(false);
-    const [payload, setPayload] = useState<MessageType | null>(null);
+    const [payload, setPayload] = useState<payloadType| null>(null);
     const channelRef = useRef<RealtimeChannel | null>(null);
+    const [roomName,setRoomName] = useState("")
     const {supabaseClient} = useSessionContext()
     useEffect(() => {
-      const newChannel = supabaseClient.channel(`typing:${roomId}`);
-  
+      const newChannel = supabaseClient.channel(`typing:${roomName}`);
       const onTyping = (payload: any) => {
         setPayload(payload);
         setIsTyping(true);
@@ -37,14 +39,14 @@ export const useTypingIndicator = ({ roomId, userId }: TypingIndicatorProps) => 
       return () => {
         subscription.unsubscribe();
       };
-    }, [roomId, userId]);
+    }, [roomName]);
   
     const throttledTypingEvent = throttle(() => {
       if (!channelRef.current) return;
       channelRef.current.send({
         type: 'broadcast',
         event: 'typing',
-        payload: { userId },
+        userAva: userAva ,
       });
     }, 3000);
   
@@ -52,5 +54,5 @@ export const useTypingIndicator = ({ roomId, userId }: TypingIndicatorProps) => 
       throttledTypingEvent();
     }, [throttledTypingEvent]);
   
-    return { payload, isTyping, sendTypingEvent };
-  };
+    return { payload, isTyping, sendTypingEvent,setRoomName };
+  }; 
