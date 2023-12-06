c6fe5c6a46c9de87d7372f25d8ed2858e57cc8f7
LostArrows27 <thelastofus2isnuts@gmail.com>
2023-12-07 01:37:05 +0700

    identify flow for live session room + update attender presence in appointment

--- src/app/live-session/page.tsx
+++ src/app/live-session/page.tsx
@@ -5,6 +5,7 @@ import { convertDurationToMinute } from "@/lib/convertDurationToMinute";
 import { isCurrentTimeAfterAppointmentEnd } from "@/lib/isCurrentTimeAfterAppointment";
 import {
   AppointmentType,
+  DetailedAppointmentType,
   SessionApplicationType,
 } from "@/types/supabaseTableType";
 import { Database } from "@/types/supabaseTypes";
@@ -17,11 +18,12 @@ export default async function LiveSessionPage({
 }: {
   searchParams: { userID: string; room: string };
 }) {
-  // Authorization Flow
+  // Authorization Flow with room = appointmentID and userID = currentUserID (coach or student)
 
   const { userID, room } = searchParams;
   const supabaseClient = createServerComponentClient<Database>({ cookies });
 
+  // identify user
   const {
     data: { user },
   } = await supabaseClient.auth.getUser();
@@ -31,26 +33,29 @@ export default async function LiveSessionPage({
     return redirect("/");
   }
 
-  //TODO: check if that room is exist (room = appointmentID)
+  // check if that room is exist (room = appointmentID)
+  // which mean that appoitment is exist in database
 
   const { data: appoinmentData, error } = (await supabaseClient
     .from("appointment")
-    .select("*, course_session(*)")
+    .select(
+      `*, 
+      coach_data:profiles!appointment_coach_profile_id_fkey(*),
+      student_data:profiles!appointment_student_id_fkey(*),
+      course_session(*)
+    `
+    )
     .eq("id", room)
     .single()) as unknown as {
-    data: AppointmentType & {
-      course_session: SessionApplicationType;
-    };
+    data: DetailedAppointmentType;
     error: any;
   };
 
-  // error or appointment not exist
+  //  redirect to home page if that room is not exist
   if (error || !appoinmentData) {
     console.error(error);
     return redirect("/");
   }
-
-  console.log(appoinmentData);
 
   // case that anyone have appointment ID but they are not identified as student or coach
   if (
@@ -59,11 +64,13 @@ export default async function LiveSessionPage({
   ) {
     return redirect("/");
   }
-  //TODO: check if that room is not expired (appointment_date > now)
+
+  // check if that room is not expired (appointment_date > now)
   if (new Date(appoinmentData.appointment_time) > new Date()) {
     return <CountDownAppointMent data={appoinmentData} />;
   }
 
+  // check if the room is expired (appointment_date + duration > now)
   if (
     isCurrentTimeAfterAppointmentEnd(
       appoinmentData.appointment_time,
@@ -72,17 +79,39 @@ export default async function LiveSessionPage({
   ) {
     return <LiveSessionEnd />;
   }
-  // const;
 
-  // const res = await fetch(
-  //   `${process.env.NEXT_PUBLIC_SITE_URL}/api/livekit?room=${searchParams.room}&username=${username}`
-  // );
-  // const data = await res.json();
-  return (
-    <div>
-      <div>room: {room}</div>
-      <div>userID: {userID}</div>
-    </div>
+  let userName;
+
+  if (appoinmentData.student_id === userID) {
+    userName = appoinmentData.student_data.name;
+  } else {
+    userName = appoinmentData.coach_data.name;
+  }
+
+  // update presence of student or coach
+  let updatePresence;
+
+  if (appoinmentData.student_id === userID) {
+    updatePresence = supabaseClient
+      .from("appointment")
+      .update({ student_verify: true })
+      .eq("id", room);
+  } else {
+    updatePresence = supabaseClient
+      .from("appointment")
+      .update({ coach_verify: true })
+      .eq("id", room);
+  }
+
+  const getToken = fetch(
+    `${process.env.NEXT_PUBLIC_SITE_URL}/api/livekit?room=${searchParams.room}&username=${userName}`
   );
-  // return <LiveSession token={data.token} />;
+
+  const [res, updatePresenceRes] = await Promise.all([
+    getToken,
+    updatePresence,
+  ]);
+  const data = await res.json();
+
+  return <LiveSession token={data.token} />;
 }
