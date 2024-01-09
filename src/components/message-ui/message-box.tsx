d564e5716967b4da1d7f38d898fb798cf74cf722
MinhMatMong <kurominhnhat@gmail.com>
2024-01-09 16:38:37 +0700

    glad to be back

--- src/components/message-ui/message-box.tsx
+++ src/components/message-ui/message-box.tsx
@@ -1,22 +1,27 @@
-import { Button } from "@/components/ui/button";
 
 import {
   Sheet,
   SheetClose,
-    SheetContent,
-    SheetDescription,
-    SheetFooter,
-    SheetHeader,
-    SheetTitle,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
+import {
+  TooltipProvider,
+  Tooltip,
+  TooltipTrigger,
+  TooltipContent,
+} from "../ui/tooltip";
 import { HiChatBubbleLeftRight } from "react-icons/hi2";
 import MessageHeadList from "./message-head-list";
 import MessageDetails from "./message-details";
 import useFriendMessages from "@/hooks/useFriendMessages";
 
 export function MessageBox() {
-  const { isOpen, onOpen,onClose} = useFriendMessages((set) => set);
+  const { isOpen, onOpen, onClose } = useFriendMessages((set) => set);
   const onChange = (open: boolean) => {
     if (!open) {
       onClose();
@@ -25,9 +30,21 @@ export function MessageBox() {
   return (
     <Sheet open={isOpen} onOpenChange={onChange}>
       <SheetTrigger asChild>
-        <Button onClick={onOpen} className="bg-transparent text-white hover:text-primary hover:bg-transparent text-4xl">
-          <HiChatBubbleLeftRight />
-        </Button>
+        <TooltipProvider delayDuration={200}>
+          <Tooltip>
+            <TooltipTrigger>
+              <div
+                onClick={onOpen}
+                className="bg-transparent text-white hover:text-primary hover:bg-transparent text-4xl"
+              >
+                <HiChatBubbleLeftRight />
+              </div>
+            </TooltipTrigger>
+            <TooltipContent side="top">
+              <p>Chat</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
       </SheetTrigger>
 
       <SheetContent className=" bg-home !min-w-[1000px] flex p-0 gap-x-0 rounded-l-3xl">
--- src/components/sideBar/sidebar-right.tsx
+++ src/components/sideBar/sidebar-right.tsx
@@ -63,7 +63,7 @@ export default function SideBarRight() {
           filter: `user_id=eq.${user?.id}`,
         },
         async (payload) => {
-          console.log(payload)
+          console.log(payload);
           if (payload.new.user_id === user?.id) {
             const { data, error } = await supabaseClient.rpc(
               "get_latest_group_messages",
@@ -82,40 +82,39 @@ export default function SideBarRight() {
     return () => {
       supabaseClient.removeChannel(channel);
     };
-  }, [user,currentGroup,supabaseClient]);
+  }, [user, currentGroup, supabaseClient]);
   return (
     <aside
       className={cn(
         "fixed  right-4 top-0 fade-in h-full py-6 flex flex-col  overflow-y-scroll gap-y-4 "
       )}
     >
-      {groupChatHeads.length > 0 && (
-        <div
-          className={cn(
-            " flex-1 bg-muted rounded-3xl flex flex-col items-center overflow-hidden   py-6  xl:px-4 px-2 "
-          )}
-        >
-          <div className="mb-6 text-4xl">
-            <GroupChatBox />
-          </div>
-          <div className="gap-y-6 scrollbar flex flex-col px-1 pt-1  overflow-y-scroll">
-            {!loading ? (
-              groupChatHeads.map((msh, ind) => {
-                return <GroupAvatar key={ind} groupHead={msh} />;
-              })
-            ) : (
-              <>
-                <GamerAvatar />
-                <GamerAvatar />
-                <GamerAvatar />
-                <GamerAvatar />
-                <GamerAvatar />
-                <GamerAvatar />
-              </>
-            )}
-          </div>
+      {" "}
+      <div
+        className={cn(
+          " flex-1 bg-muted rounded-3xl flex flex-col items-center overflow-hidden   py-6  xl:px-4 px-2 "
+        )}
+      >
+        <div className="mb-6 text-4xl">
+          <GroupChatBox />
         </div>
-      )}
+        <div className="gap-y-6 scrollbar flex flex-col px-1 pt-1 overflow-y-auto h-full">
+          {!loading ? (
+            groupChatHeads.map((msh, ind) => {
+              return <GroupAvatar key={ind} groupHead={msh} />;
+            })
+          ) : (
+            <>
+              <GamerAvatar />
+              <GamerAvatar />
+              <GamerAvatar />
+              <GamerAvatar />
+              <GamerAvatar />
+              <GamerAvatar />
+            </>
+          )}
+        </div>
+      </div>
       <div
         className={cn(
           " flex-1 bg-muted rounded-3xl flex flex-col items-center overflow-hidden py-6  xl:px-4 px-2  "
