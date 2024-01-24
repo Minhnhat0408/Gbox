b5d370f6d4de9780c33a0900fda1dcfb8c2e6762
LostArrows27 <thelastofus2isnuts@gmail.com>
2024-01-24 16:21:12 +0700

    delete event function

--- src/components/events-detail-section/EventControlMenu.tsx
+++ src/components/events-detail-section/EventControlMenu.tsx
@@ -9,30 +9,111 @@ import {
 import { Button } from "../ui/button";
 import { BsThreeDots } from "react-icons/bs";
 import { useEventDetail } from "@/hooks/useEventDetail";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { useState } from "react";
+import { toast } from "sonner";
+import { useSupabase } from "@/hooks/useSupabaseClient";
+import { useRouter } from "next/navigation";
+import { wait } from "@/lib/wait";
+import { AiOutlineLoading } from "react-icons/ai";
 
 const EventControlMenu = () => {
-  const {} = useEventDetail();
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { id } = useEventDetail();
+  const supabase = useSupabase();
+  const router = useRouter();
   // TODO: edit information + notification to all participants
 
   // TODO: delete event
+  const handleDeleteEvent = async () => {
+    try {
+      setLoading(true);
+
+      const { data, error } = await supabase
+        .from("events")
+        .delete()
+        .eq("id", id);
+
+      if (error) throw error;
+
+      setLoading(false);
+      setOpen(false);
+      toast.success("Event deleted successfully");
+      router.push("/events");
+    } catch (error: any) {
+      toast.error(error.message);
+      setLoading(false);
+    }
+  };
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button size="sm" className="">
-          <BsThreeDots className="text-xl text-white" />
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>Options</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Edit Event Detail</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Delete Event</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size="sm" className="">
+            <BsThreeDots className="text-xl text-white" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuLabel>Options</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Edit Event Detail</DropdownMenuItem>
+          <DropdownMenuItem
+            onClick={() => {
+              setOpen(true);
+            }}
+          >
+            Delete Event
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <AlertDialog open={open}>
+        <AlertDialogTrigger asChild></AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogDescription>
+              This action cannot be undone. This will permanently delete your
+              account and remove your data from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              className="w-[110px]"
+              onClick={handleDeleteEvent}
+            >
+              {loading ? (
+                <>
+                  <span>Delete </span>
+                  <AiOutlineLoading className="animate-spin text-xl ml-2" />
+                </>
+              ) : (
+                <span>Delete </span>
+              )}
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
   );
 };
 
